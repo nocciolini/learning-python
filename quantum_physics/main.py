4f3366cb9a6643c32f6e59a40cf7b6b9d2da568d
nocciolini <leo.nocciolini@gmail.com>
2024-11-28 23:35:45 +0000

    Adding new exercise

---
+++ quantum_physics/main.py
@@ -0,0 +1,31 @@
+def mean(numbers:list) -> float:
+    return sum(numbers) / len(numbers)
+
+def median(numbers: list) -> float:
+    n = len(numbers)
+    sorted_numbers = sorted(numbers)
+
+    if n % 2 == 0:
+        return (sorted_numbers[n//2] + sorted_numbers[n//2 - 1])/2
+    else:
+        return sorted_numbers[n//2]
+
+def variance(numbers: list) -> float:
+    average = mean(numbers)
+
+
+
+size = int(input("Type the amount of numbers you intent to insert: "))
+
+numberList = []
+for i in range(size):
+    numberList.append(int(input("Input the number: ")))
+
+print("Mean:", mean(numberList))
+print("Median:", median(numberList))
+print("Maximum:", max(numberList))
+print("Minimum:", min(numberList))
+print("Variance:", variance(numberList))
+
+
+
